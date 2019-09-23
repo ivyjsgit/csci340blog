ae12b332cd2f5c2732576cad9a908daf2bb145e7
Ivy Jackson <ivyjs@hendrix.edu>
2019-09-23 08:59:21 -0500

    Added Jekyll Pages

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,268 +1,262 @@
-/*
-	Massively by HTML5 UP
-	html5up.net | @ajlkn
-	Free for personal and commercial use under the CCA 3.0 license (html5up.net/license)
-*/
-
-(function($) {
-
-	skel.breakpoints({
-		xlarge:	'(max-width: 1680px)',
-		large:	'(max-width: 1280px)',
-		medium:	'(max-width: 980px)',
-		small:	'(max-width: 736px)',
-		xsmall:	'(max-width: 480px)',
-		xxsmall: '(max-width: 360px)'
-	});
-
-	/**
-	 * Applies parallax scrolling to an element's background image.
-	 * @return {jQuery} jQuery object.
-	 */
-	$.fn._parallax = function(intensity) {
-
-		var	$window = $(window),
-			$this = $(this);
-
-		if (this.length == 0 || intensity === 0)
-			return $this;
-
-		if (this.length > 1) {
-
-			for (var i=0; i < this.length; i++)
-				$(this[i])._parallax(intensity);
-
-			return $this;
-
-		}
-
-		if (!intensity)
-			intensity = 0.25;
-
-		$this.each(function() {
-
-			var $t = $(this),
-				$bg = $('<div class="bg"></div>').appendTo($t),
-				on, off;
-
-			on = function() {
-
-				$bg
-					.removeClass('fixed')
-					.css('transform', 'matrix(1,0,0,1,0,0)');
-
-				$window
-					.on('scroll._parallax', function() {
-
-						var pos = parseInt($window.scrollTop()) - parseInt($t.position().top);
-
-						$bg.css('transform', 'matrix(1,0,0,1,0,' + (pos * intensity) + ')');
-
-					});
-
-			};
-
-			off = function() {
-
-				$bg
-					.addClass('fixed')
-					.css('transform', 'none');
-
-				$window
-					.off('scroll._parallax');
-
-			};
-
-			// Disable parallax on ..
-				if (skel.vars.browser == 'ie'		// IE
-				||	skel.vars.browser == 'edge'		// Edge
-				||	window.devicePixelRatio > 1		// Retina/HiDPI (= poor performance)
-				||	skel.vars.mobile)				// Mobile devices
-					off();
-
-			// Enable everywhere else.
-				else {
-
-					skel.on('!large -large', on);
-					skel.on('+large', off);
-
-				}
-
-		});
-
-		$window
-			.off('load._parallax resize._parallax')
-			.on('load._parallax resize._parallax', function() {
-				$window.trigger('scroll');
-			});
-
-		return $(this);
-
-	};
-
-	$(function() {
-
-		var	$window = $(window),
-			$body = $('body'),
-			$wrapper = $('#wrapper'),
-			$header = $('#header'),
-			$nav = $('#nav'),
-			$main = $('#main'),
-			$navPanelToggle, $navPanel, $navPanelInner;
-
-		// Disable animations/transitions until the page has loaded.
-			$window.on('load', function() {
-				window.setTimeout(function() {
-					$body.removeClass('is-loading');
-				}, 100);
-			});
-
-		// Prioritize "important" elements on medium.
-			skel.on('+medium -medium', function() {
-				$.prioritize(
-					'.important\\28 medium\\29',
-					skel.breakpoint('medium').active
-				);
-			});
-
-		// Scrolly.
-			$('.scrolly').scrolly();
-
-		// Background.
-			$wrapper._parallax(0.925);
-
-		// Nav Panel.
-
-			// Toggle.
-				$navPanelToggle = $(
-					'<a href="#navPanel" id="navPanelToggle">Menu</a>'
-				)
-					.appendTo($wrapper);
-
-				// Change toggle styling once we've scrolled past the header.
-					$header.scrollex({
-						bottom: '5vh',
-						enter: function() {
-							$navPanelToggle.removeClass('alt');
-						},
-						leave: function() {
-							$navPanelToggle.addClass('alt');
-						}
-					});
-
-			// Panel.
-				$navPanel = $(
-					'<div id="navPanel">' +
-						'<nav>' +
-						'</nav>' +
-						'<a href="#navPanel" class="close"></a>' +
-					'</div>'
-				)
-					.appendTo($body)
-					.panel({
-						delay: 500,
-						hideOnClick: true,
-						hideOnSwipe: true,
-						resetScroll: true,
-						resetForms: true,
-						side: 'right',
-						target: $body,
-						visibleClass: 'is-navPanel-visible'
-					});
-
-				// Get inner.
-					$navPanelInner = $navPanel.children('nav');
-
-				// Move nav content on breakpoint change.
-					var $navContent = $nav.children();
-
-					skel.on('!medium -medium', function() {
-
-						// NavPanel -> Nav.
-							$navContent.appendTo($nav);
-
-						// Flip icon classes.
-							$nav.find('.icons, .icon')
-								.removeClass('alt');
-
-					});
-
-					skel.on('+medium', function() {
-
-						// Nav -> NavPanel.
-						$navContent.appendTo($navPanelInner);
-
-						// Flip icon classes.
-							$navPanelInner.find('.icons, .icon')
-								.addClass('alt');
-
-					});
-
-				// Hack: Disable transitions on WP.
-					if (skel.vars.os == 'wp'
-					&&	skel.vars.osVersion < 10)
-						$navPanel
-							.css('transition', 'none');
-
-		// Intro.
-			var $intro = $('#intro');
-
-			if ($intro.length > 0) {
-
-				// Hack: Fix flex min-height on IE.
-					if (skel.vars.browser == 'ie') {
-						$window.on('resize.ie-intro-fix', function() {
-
-							var h = $intro.height();
-
-							if (h > $window.height())
-								$intro.css('height', 'auto');
-							else
-								$intro.css('height', h);
-
-						}).trigger('resize.ie-intro-fix');
-					}
-
-				// Hide intro on scroll (> small).
-					skel.on('!small -small', function() {
-
-						$main.unscrollex();
-
-						$main.scrollex({
-							mode: 'bottom',
-							top: '25vh',
-							bottom: '-50vh',
-							enter: function() {
-								$intro.addClass('hidden');
-							},
-							leave: function() {
-								$intro.removeClass('hidden');
-							}
-						});
-
-					});
-
-				// Hide intro on scroll (<= small).
-					skel.on('+small', function() {
-
-						$main.unscrollex();
-
-						$main.scrollex({
-							mode: 'middle',
-							top: '15vh',
-							bottom: '-15vh',
-							enter: function() {
-								$intro.addClass('hidden');
-							},
-							leave: function() {
-								$intro.removeClass('hidden');
-							}
-						});
-
-				});
-
-			}
-
-	});
-
-})(jQuery);
+jQuery(document).ready(function () {
+
+    if (is_touch_device())
+    {
+        jQuery('.full-screen-scroll article').css('opacity', '1', '!important');
+    }
+
+    var ua = navigator.userAgent.toLowerCase();
+    if ((ua.indexOf("safari/") !== -1 && ua.indexOf("windows") !== -1 && ua.indexOf("chrom") === -1) || is_touch_device())
+    {
+
+        jQuery("html").css('overflow', 'auto');
+
+        jQuery(".scroll-top").click(function () {
+            jQuery('html, body').animate({scrollTop: 0}, 2000);
+            return false;
+        });
+
+    } else
+    {
+        jQuery("html, .menu-left-part, #cbp-bislideshow.scroll").niceScroll({cursorcolor: "#5B5B5B", scrollspeed: 100, mousescrollstep: 80, cursorwidth: "12px", cursorborder: "none", cursorborderradius: "0px"});
+
+        //Scroll Top animation
+        jQuery(".scroll-top").click(function () {
+            jQuery("html").getNiceScroll(0).doScrollTop(0);
+        });
+
+        jQuery(".sidebar").mouseover(function () {
+            jQuery(".menu-left-part").getNiceScroll().resize();
+        });
+    }
+
+
+    //Placeholder show/hide
+    jQuery('input, textarea').focus(function () {
+        jQuery(this).data('placeholder', jQuery(this).attr('placeholder'));
+        jQuery(this).attr('placeholder', '');
+    });
+    jQuery('input, textarea').blur(function () {
+        jQuery(this).attr('placeholder', jQuery(this).data('placeholder'));
+    });
+
+
+// preload the images
+    jQuery('#cbp-bislideshow.scroll').imagesLoaded(function () {
+        var count = 0;
+        var scrollItemWidth = jQuery('.cbp-bislideshow.scroll li').outerWidth();
+        jQuery('#cbp-bislideshow.scroll').children('li').each(function () {
+            var $item = jQuery(this);
+            $item.css({'left': count * scrollItemWidth});
+            count++;
+        });
+    });
+
+    //Fix for default menu
+    jQuery('.default-menu ul').addClass('main-menu sm sm-clean');
+
+});
+
+
+
+jQuery(window).load(function () {
+
+    jQuery(".blog-item-holder").hover(function () {
+        jQuery(".blog-item-holder").not(this).addClass('blur');
+    },
+            function () {
+                jQuery(".blog-item-holder").removeClass('blur');
+            });
+
+
+//Set menu
+    jQuery('.main-menu').smartmenus({
+        subMenusSubOffsetX: 1,
+        subMenusSubOffsetY: -8,
+        markCurrentItem: true
+    });
+
+
+//Set each image slider
+    jQuery(".image-slider").each(function () {
+        var id = jQuery(this).attr('id');
+        if (window[id + '_pagination'] == 'true')
+        {
+            var pagination_value = '.' + id + '_pagination';
+        } else
+        {
+            var pagination_value = false;
+        }
+
+        var auto_value = window[id + '_auto'];
+        if (auto_value == 'false')
+        {
+            auto_value = false;
+        } else {
+            auto_value = true;
+        }
+
+        var hover_pause = window[id + '_hover'];
+        if (hover_pause == 'true')
+        {
+            hover_pause = 'resume';
+        } else {
+            hover_pause = false;
+        }
+
+        var speed_value = window[id + '_speed'];
+
+        jQuery('#' + id).carouFredSel({
+            responsive: true,
+            width: 'variable',
+            auto: {
+                play: auto_value,
+                pauseOnHover: hover_pause
+            },
+            pagination: pagination_value,
+            scroll: {
+                fx: 'crossfade',
+                duration: parseFloat(speed_value)
+            },
+            swipe: {
+                onMouse: true,
+                onTouch: true
+            },
+            items: {
+                height: 'variable'
+            }
+        });
+    });
+
+    jQuery('.carousel_pagination').each(function () {
+        var pagination_width = jQuery(this).width();
+        var windw_width = jQuery('.image-slider-wrapper').width();
+        jQuery(this).css("margin-left", (windw_width - pagination_width) / 2);
+    });
+
+
+//Show-Hide header sidebar
+    jQuery('#toggle').on("click", multiClickFunctionStop);
+    
+    //Fix for sidebar height
+    jQuery("#sidebar").css('minHeight', jQuery("#content").outerHeight());
+
+    jQuery('.doc-loader').fadeOut('fast');
+});
+
+
+jQuery(window).resize(function () {
+
+//Fix for sidebar height
+    jQuery("#sidebar").css('minHeight', jQuery("#content").outerHeight());
+
+    jQuery('.menu-left-part.open').width(jQuery('.sidebar.open').width() - jQuery('.menu-right-part.open').width());
+
+    var count = 0;
+    var scrollItemWidth = jQuery('.cbp-bislideshow.scroll li').outerWidth();
+    jQuery('#cbp-bislideshow.scroll').children('li').each(function () {
+        var $item = jQuery(this);
+        $item.css({'left': count * scrollItemWidth});
+        count++;
+    });
+
+    jQuery('.carousel_pagination').each(function () {
+        var pagination_width = jQuery(this).width();
+        var windw_width = jQuery('.image-slider-wrapper').width();
+        jQuery(this).css("margin-left", (windw_width - pagination_width) / 2);
+    });
+
+});
+
+//------------------------------------------------------------------------
+//Helper Methods -->
+//------------------------------------------------------------------------
+
+
+var multiClickFunctionStop = function (e) {
+    e.preventDefault();
+    jQuery('#toggle').off("click");
+    jQuery('#toggle').toggleClass("on");
+    jQuery('html, body, .sidebar, .menu-left-part, .menu-right-part').toggleClass("open");
+    jQuery('.menu-left-part').width('320px');
+    jQuery('.menu-left-part.open').width(jQuery('.sidebar.open').width() - jQuery('.menu-right-part.open').width());
+    jQuery('#toggle').on("click", multiClickFunctionStop);
+};
+
+function is_touch_device() {
+    return !!('ontouchstart' in window);
+}
+
+jQuery(window).bind("scroll", function () {
+    if (jQuery(this).scrollTop() > 700) {
+        jQuery('.scroll-top').fadeIn(500);
+    } else
+    {
+        jQuery('.scroll-top').fadeOut(500);
+    }
+});
+
+function isValidEmailAddress(emailAddress) {
+    var pattern = /^([a-z\d!#$%&'*+\-\/=?^_`{|}~\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]+(\.[a-z\d!#$%&'*+\-\/=?^_`{|}~\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]+)*|"((([ \t]*\r\n)?[ \t]+)?([\x01-\x08\x0b\x0c\x0e-\x1f\x7f\x21\x23-\x5b\x5d-\x7e\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]|\\[\x01-\x09\x0b\x0c\x0d-\x7f\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]))*(([ \t]*\r\n)?[ \t]+)?")@(([a-z\d\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]|[a-z\d\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF][a-z\d\-._~\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]*[a-z\d\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])\.)+([a-z\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]|[a-z\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF][a-z\d\-._~\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]*[a-z\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])\.?$/i;
+    return pattern.test(emailAddress);
+}
+
+
+var SendMail = function () {
+
+    var emailVal = jQuery('#contact-email').val();
+
+    if (isValidEmailAddress(emailVal)) {
+        var params = {
+            'action': 'SendMessage',
+            'name': jQuery('#name').val(),
+            'email': jQuery('#contact-email').val(),
+            'subject': jQuery('#subject').val(),
+            'message': jQuery('#message').val()
+        };
+        jQuery.ajax({
+            type: "POST",
+            url: "php/sendMail.php",
+            data: params,
+            success: function (response) {
+                if (response) {
+                    var responseObj = jQuery.parseJSON(response);
+                    if (responseObj.ResponseData)
+                    {
+                        alert(responseObj.ResponseData);
+                    }
+                }
+            },
+            error: function (xhr, ajaxOptions, thrownError) {
+                //xhr.status : 404, 303, 501...
+                var error = null;
+                switch (xhr.status)
+                {
+                    case "301":
+                        error = "Redirection Error!";
+                        break;
+                    case "307":
+                        error = "Error, temporary server redirection!";
+                        break;
+                    case "400":
+                        error = "Bad request!";
+                        break;
+                    case "404":
+                        error = "Page not found!";
+                        break;
+                    case "500":
+                        error = "Server is currently unavailable!";
+                        break;
+                    default:
+                        error = "Unespected error, please try again later.";
+                }
+                if (error) {
+                    alert(error);
+                }
+            }
+        });
+    } else
+    {
+        alert('Your email is not in valid format');
+    }
+};
--- things-that-makes-me-unhappy-at-hendrix.md
+++ things-that-makes-me-unhappy-at-hendrix.md
@@ -1,8 +1,10 @@
 ---
+layout: markdown
 layout: page
 title: Things that make me unhappy at hendrix
 permalink: /things-that-make-me-unhappy-at-hendrix/
 ---
+
 1. The cutting down of so much of the Hendrix nature preseve. 
 The Hendrix nature preserve used to be a beautiful place where you could view flora and fauna in their natural habitat. This changed over the summer break, when most of this was removed in order to make room for woodchips and emptiness on the ground.
 2. The tiles on the side of the Creative Quad. While they do remind the students/alumni/donors of Huelen Hall (which used to stand there), the tiles appear to be covered in moss and other plant life. This isn't very aesthetically pleasing. 
